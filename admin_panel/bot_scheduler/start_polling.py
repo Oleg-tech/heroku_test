8b46470352fedb527210edf9583e61da91e641c5
chel121212 <64197137+Oleg-tech@users.noreply.github.com>
2022-10-22 10:17:33 +0300

    m

--- admin_panel/bot_scheduler/start_polling.py
+++ admin_panel/bot_scheduler/start_polling.py
@@ -1,9 +1,9 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 
-from .bot_controller import main_polling
+# from .bot_controller import main_polling
 
 
 def start():
     scheduler = BackgroundScheduler(timezone='Europe/Kiev')
-    scheduler.add_job(main_polling)
-    scheduler.start()
+    # scheduler.add_job(main_polling)
+    # scheduler.start()
